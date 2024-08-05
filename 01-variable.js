5158efa4752ce0689a3d797846273b58b5d051a9
alhadthakare09 <alhadthakare09@gmail.com>
2024-08-05 16:28:57 +0530

    javascript variable
    javascript variable

---
+++ 01-variable.js
@@ -0,0 +1,19 @@
+//variable declaration
+
+var firstName;
+console.log(firstName);
+
+//variable initialization
+
+firstName ="jenny";
+console.log(firstName);
+//variable declaration and initialization
+
+var lastname = "kumar"
+console.log(lastname);
+console.log("My lastname is:",lastname);
+
+
+//variable update
+firstName = "jennifer";
+console.log(firstName);
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Hello, Alhad");
+//its my first program
+console.log("My city Name is Pune");
+console.log("My best friend name is xyz");
+console.log("My Country Name is India");
+console.log("My School Name is xyz");
+console.log("My College Name is jd");
