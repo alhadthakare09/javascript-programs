2ffdc96c02bcdd5dd8d8a6e00ad522df3796c0a9
alhadthakare09 <alhadthakare09@gmail.com>
2024-08-05 17:34:22 +0530

    assignment 1 and 2
    assignment 1 and 2

---
+++ 01-assignment.js
@@ -0,0 +1,35 @@
+console.log("===========1==========");
+
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Alhad Thakare";
+console.log("My full name is:",myFullName);
+
+var myFullName = "Alhad Avinash Thakare";
+console.log("My middle name is:",myFullName);
+
+console.log("===========2==========");
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log("===========3==========");
+
+var firstName = "Alhad";
+console.log(firstName);
+var lastName = "Thakare";
+console.log(lastName);
+var age = "20";
+console.log(age);
+var address = "Pune";
+console.log(address);
+var pinCode = "Alhad"
+console.log(pinCode);
+var gender = "male";
+console.log(gender);
+var isMarried = false;
+console.log(isMarried);
+
+
+
---
+++ 02-assignment.js
@@ -0,0 +1,54 @@
+console.log("==========1========");
+
+var bankName ;
+console.log(bankName);
+
+var bankName = "SBI Bank";
+console.log( "Bankname is :",bankName);
+
+var type = typeof bankName;
+console.log("Type is:",type);
+
+console.log("==========2========");
+
+var marks = '90%';
+console.log("marks are:",marks);
+var type = typeof marks;
+console.log("The type is :",type);
+
+console.log("==========3========");
+
+var isWorking ;
+console.log("before initialization:",isWorking);
+
+var isWorking= true;
+console.log("after initialization:",isWorking);
+
+var type = typeof isWorking;
+console.log("type is :",type);
+
+console.log("==========4========");
+
+var totalCount = "Hundred And Seven";
+console.log("the total count is :",totalCount);
+var type = typeof totalCount;
+console.log("the type is:",type);
+var totalCount = 107;
+console.log("the total count is :",totalCount);
+var type = typeof totalCount;
+console.log("the type is:",type);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
